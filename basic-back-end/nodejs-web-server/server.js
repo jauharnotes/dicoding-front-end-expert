7f4d19ea57059dc19be3c80da628300eb4748739
jauharnotes <jauharnotes@gmail.com>
2022-02-02 21:14:46 +0700

    request listener

--- basic-back-end/nodejs-web-server/server.js
+++ basic-back-end/nodejs-web-server/server.js
@@ -1,1 +1,50 @@
-console.log('Halo, kita akan membuat server');
+const http = require("http");
+
+const requesListener = (request, response) => {
+    response.setHeader("Context-Type", "text/html");
+
+    const { method, url } = request;
+
+    if (url === "/") {
+        if (method === "GET") {
+            response.statusCode = 200;
+            response.end("<h1>Ini adalah halaman homepage!</h1>");
+        } else {
+            response.statusCode = 400;
+            response.end(`<h1>Halaman tidak dapat diakses ${method} request</h1>`);
+        }
+    } else if (url === "/about") {
+        if (method === "GET") {
+            response.statusCode = 200;
+            response.end("<h1>Ini adalah halaman about</h1>");
+        } else if (method === "POST") {
+            let body = [];
+
+            request.on("data", (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on("end", () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about`);
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(`<h1>Halaman tidak dapat diakses ${method} request</h1>`);
+        }
+    } else {
+        response.statusCode = 404;
+        response.end("<h1>Halaman tidak ditemukan!</h1>");
+    }
+};
+
+const server = http.createServer(requesListener);
+
+const port = 5000;
+const host = "localhost";
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
