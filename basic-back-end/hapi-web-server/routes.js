ec75d62d8aa669a40de9b4ad35915b1899dd5360
jauharnotes <jauharnotes@gmail.com>
2022-02-03 19:01:03 +0700

    hepi nodejs freamwork

--- basic-back-end/hapi-web-server/routes.js
+++ basic-back-end/hapi-web-server/routes.js
@@ -28,6 +28,20 @@ const routes = [
         },
     },
     {
+        method: "GET",
+        path: "/hello/{name?}",
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            const { lang } = request.query;
+
+            if (lang === "id") {
+                return `Hi, ${name}`
+            }
+
+            return `Hello, ${name}`
+        }
+    },
+    {
         method: "*",
         path: "/{any}",
         handler: (request, h) => {
